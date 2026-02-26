f9905481a78ee6f083d0ded1dfdd5a3ba411785a
biprodas050-hub <biprodas050@gmail.com>
2026-02-26 21:14:06 +0600

    Update fifth.dart

--- fifth.dart
+++ fifth.dart
@@ -2,11 +2,9 @@ import 'dart:io';
 
 void main() async {
   String fileName = "hello.txt";
-  String name = "John Doe"; // Replace with your name
+  String name = "John Doe"; 
 
   File file = File(fileName);
-
-  // Append name to the file
   await file.writeAsString("$name\n", mode: FileMode.append);
 
   print("Name added to $fileName successfully.");
